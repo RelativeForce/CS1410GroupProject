5bceee498949f54191cf9b8b6e8198c53f3603e2
Adrian Wong <Adrian Wong@10.76.7.247>
2017-03-27 15:19:13 +0100

    Development (27/03/17)
    [CHANGES]
    1. Implemented fully the Truck, SamllCar, Motorbike and FamiltSedan
    RoadUsers. 
    2. Added the ability to clone all road users types.

--- src/environment/model/roadusers/Truck_RoadUser.java
+++ src/environment/model/roadusers/Truck_RoadUser.java
@@ -4,39 +4,53 @@ import java.util.Random;
 
 import environment.model.roadusers.vehicles.Truck_Vehicle;
 
-public class Truck_RoadUser extends RoadUser {
+public class Truck_RoadUser extends RoadUser implements Cloneable {
 
 	private static final double PROB_TO_SHOP = 1.0;
-	private static final double SPENDING_RANGE = 5;
-	private static final double MINIMUM_SPENDING_MONEY = 15;
-	private static final double MAXIMUM_SPENDING_MONEY = 20;
+	private static final int SPENDING_RANGE = 5;
+	private static final int MINIMUM_SPENDING_MONEY = 15;
 	private static final int SHOPPING_TIME_RANGE = 18;
 	private static final int MINIMUM_SHOPPING_TIME = 24;
 	private static final int MAXIMUM_TIME_TO_SHOP = 36;
-	private static double t = 0.02;
+	private static final double INITIAL_T = 0.02;
+	
+	private static double t = INITIAL_T;
+
+	private static final Random GEN = new Random();
 
 	public Truck_RoadUser() {
-		super(new Truck_Vehicle());
+		super(new Truck_Vehicle(), GEN.nextInt(SHOPPING_TIME_RANGE) + MINIMUM_SHOPPING_TIME,
+				GEN.nextInt(SPENDING_RANGE) + MINIMUM_SPENDING_MONEY);
 	}
 
 	public static boolean exists(double p, double q, double value) {
 		return (value > ((2 * p) + q)) && (value <= ((2 * p) + q + t));
 	}
 
-	public void shop() {
-		if (timeSpent < 48) {
-			Random rn = new Random();
-			double d = rn.nextDouble();
-			if (d <= PROB_TO_SHOP) {
-				// shop();
-			}
+	@Override
+	public boolean willShop() {
+
+		double prob = GEN.nextDouble();
+		boolean belowMaxTime = (timeSpent < MAXIMUM_TIME_TO_SHOP);
+
+		if (!belowMaxTime) {
+			t = t * 0.8;
+		} else if(belowMaxTime && t < INITIAL_T){
+			t = t * 0.05;
 		}
+
+		return belowMaxTime && (prob <= PROB_TO_SHOP);
 	}
 
 	@Override
 	public double getWorth() {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	public Truck_RoadUser clone(){
+		Truck_RoadUser copy = (Truck_RoadUser) super.createClone(new Truck_RoadUser());
+		return copy;
 	}
 
 }
