ffb876e0fb2c523380799f05a39ca6dba4b1a11a
JohnBeeBlue <john.berg.96bits@gmail.com>
2017-04-04 13:38:33 +0100

    TEST!!!

---
+++ src-test/environment/model/locations/testShoppingArea.java
@@ -0,0 +1,44 @@
+package environment.model.locations;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import environment.model.locations.*;
+import environment.model.roadusers.Motorbike_RoadUser;
+import environment.model.roadusers.RoadUser;
+
+public class testShoppingArea {
+
+	Location testShoppingArea;
+	
+	@Before
+	public void setUp() throws Exception {
+		
+		testShoppingArea = new ShoppingArea(Till.class);
+		
+	}
+
+	@Test
+	public void testRoadUsersProcessed() {
+
+			assertTrue(testShoppingArea.getRoadUsersProcessed() == 0);
+			
+			testShoppingArea.enter(new Motorbike_RoadUser());
+			
+			assertTrue(testShoppingArea.getRoadUsersProcessed() == 0);
+			
+			Map<RoadUser, Location> toMove = new HashMap<RoadUser, Location>();
+			
+			testShoppingArea.processQueue(toMove);
+			
+			assertTrue(testShoppingArea.getRoadUsersProcessed() == 1);
+			
+
+		}
+
+}
---
+++ src-test/environment/model/locations/testTill.java
@@ -0,0 +1,25 @@
+package environment.model.locations;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import environment.model.locations.Location;
+import environment.model.locations.ShoppingArea;
+import environment.model.locations.Till;
+
+public class testTill {
+
+	@Before
+	public void setUp() throws Exception {
+		
+		Location testTill = new Till(ShoppingArea.class);
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
