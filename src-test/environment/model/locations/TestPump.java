ef100f44fc5bc9c95c64eed3a12523f94b32b473
SAINI <SAINI@SAINI-VAIO>
2017-05-02 13:59:22 +0100

    Added Tests to TestPump

--- src-test/environment/model/locations/TestPump.java
+++ src-test/environment/model/locations/TestPump.java
@@ -8,10 +8,12 @@ import java.util.Map;
 import org.junit.Test;
 
 import environment.model.locations.TestLocation;
+import environment.model.Station;
 import environment.model.locations.Pump;
 import environment.model.locations.ShoppingArea;
 import environment.model.roadusers.Motorbike_RoadUser;
 import environment.model.roadusers.RoadUser;
+import environment.model.roadusers.vehicles.Vehicle;
 
 /**
  * 
@@ -20,54 +22,135 @@ import environment.model.roadusers.RoadUser;
  * 
  * @version 24/04/17
  */
-public class TestPump extends TestLocation{
+public class TestPump extends TestLocation {
 
 	private static Pump testPump = new Pump(ShoppingArea.class);
-	
+
 	public TestPump() {
 		super(testPump);
 	}
 
+	/**
+	 * This test assert that at a {@link Pump} {@link RoadUser}s will:
+	 * <ul>
+	 * <li>Fill up their {@link Vehicle}'s tank if are at the front of the
+	 * queue</li>
+	 * <li>Increment the amount of time they have spent in the {@link Station}
+	 * every time {@link Location#processQueue(Map)} is called.</li>
+	 * <li>Only be removed from the front of the queue if the {@link RoadUser}
+	 * has paid.</li>
+	 * </ul>
+	 */
 	@Test
 	public void testProcessQueue() {
 
+		testPump.queue.clear();
+
+		testFillFrontRoadUser();
+
+		testRoadUserHasPaid();
+		
+		testTimeIncrements();
+
+	}
+
+	/**
+	 * Tests whether pump increments the time spent for all of the road users
+	 * inside it each time it is processed.
+	 * 
+	 * Sub-Test {@link #testProcessQueue()}
+	 */
+	private void testTimeIncrements() {
+
+		testPump.queue.clear();
+		
+		RoadUser testRoadUser = new Motorbike_RoadUser();
+		
+		testPump.enter(testRoadUser);
+		
+		
+		
+		
+	}
+
+	/**
+	 * Test that a valid {@link RoadUser} can be added to {@link Pump}
+	 * and that it is filled when it is at the front of the queue.
+	 * 
+	 * Sub-Test {@link #testProcessQueue()}
+	 *            
+	 */
+	private void testFillFrontRoadUser() {
+
+		// A valid road user
+		RoadUser testRoadUser = new Motorbike_RoadUser();
+		
 		// Assert that the number of road users that testPump has processed is
 		// initially zero.
 		assertTrue(testPump.getRoadUsersProcessed() == 0);
 
-		RoadUser testRoadUser = new Motorbike_RoadUser();
-
+		// Add the valid parameter road user to the station where it should by
+		// default be at the front of the queue.
 		testPump.enter(testRoadUser);
 
+		// Assert that the road user that was added has not been immediately
+		// removed from the pump.
 		assertTrue(testPump.getRoadUsersProcessed() == 0);
 
+		// Imitates the toMove map in Station
 		Map<RoadUser, Location> toMove = new HashMap<RoadUser, Location>();
 
+		// Repeatedly process the pump until the road user is added to toMove.
 		while (toMove.isEmpty()) {
 			testPump.processQueue(toMove);
 		}
 
+		// Assert that the pump has not processed any road users as for a road
+		// user to be processed it must have paid for its fuel.
 		assertTrue(testPump.getRoadUsersProcessed() == 0);
 
-		assertTrue(testRoadUser.getVehicle().isFull());
+		// Assert that the pump has one vehicle in it.
+		assertTrue(testPump.queue.size() == 1);
+
+		// Assert that the vehicle at the front of the queue has a full tank.
+		assertTrue(testPump.queue.get(0).getVehicle().isFull());
+
+		// Assert that the road user who's vehicle is at the front of the queue
+		// has not paid yet.
+		assertTrue(!testPump.queue.get(0).hasPaid());
+
+	}
+
+	/**
+	 * Tests that a valid parameter {@link RoadUser} will only be removed from
+	 * the front of the queue once it has paid.
+	 * 
+	 * Sub-Test {@link #testProcessQueue()}
+	 */
+	private void testRoadUserHasPaid() {
 
 		assertTrue(testPump.queue.size() == 1);
 
-		assertTrue(!testRoadUser.doneShopping());
+		// Causes the road user at the front of the queue to pay.
+		testPump.queue.get(0).pay();
 
-		assertTrue(!testRoadUser.isShopping());
+		// Imitates the toMove map in Station
+		Map<RoadUser, Location> toMove = new HashMap<RoadUser, Location>();
 
-		assertTrue(!testRoadUser.hasPaid());
-
-		toMove.remove(testRoadUser);
-
-		assertTrue(toMove.isEmpty());
-
-		testRoadUser.shop();
-
+		// In order for the pump to remove the road user at the front it must be
+		// processed.
 		testPump.processQueue(toMove);
 
-		assertTrue(toMove.isEmpty());
+		// Now that the queue has been processed the road user that was at the
+		// front of the must have been removed leaving the queue empty.
+		assertTrue(testPump.queue.size() == 0);
+
+		// There should be 1 road user processed.
+		assertTrue(testPump.getRoadUsersProcessed() == 1);
+
+		// The road user should have been deleted as a copy of it is else where
+		// in the station.
+		assertTrue(toMove.size() == 0);
 
 	}
 
