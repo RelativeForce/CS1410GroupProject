593aa2844bf0b11a0da2b2874dde7b3a307a3542
RelativeForce <joshiceddy@hotmail.com>
2017-05-02 12:21:42 +0100

    test enter()

--- src-test/environment/model/locations/TestLocation.java
+++ src-test/environment/model/locations/TestLocation.java
@@ -2,11 +2,156 @@ package environment.model.locations;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
+import java.util.LinkedList;
+
 import org.junit.Test;
 
+import environment.model.roadusers.RoadUser;
+import environment.model.roadusers.SmallCar_RoadUser;
+
+/**
+ * 
+ * This is the test class for {@link Location}, It tests the:
+ * 
+ * <ul>
+ * <li>{@link Location#enter(RoadUser)}</li>
+ * <li>{@link Location#returnToQueue(RoadUser)}</li>
+ * <li>{@link Location#clone()}</li>
+ * </ul>
+ * 
+ * All test classes for the subclasses of {@link Location} must extend from
+ * this. {@link #TestLocation(Location)} describes how this test class is
+ * implemented in the testing subclasses.
+ * 
+ * @author Joshua_Eddy
+ * 
+ * @version 02/05/2017
+ * 
+ * @see environment.model.locations.Location
+ *
+ */
 public abstract class TestLocation {
 
+	private Location testLocation;
+
+	/**
+	 * This constructs an new {@link TestLocation}. Calling this constructor
+	 * initialises the parameter {@link Location} as the {@link Location} that
+	 * all the tests contained with {@link TestLocation} will be performed on as
+	 * these are the basic functions of a {@link Location}.
+	 * 
+	 * @param testLocation
+	 *            An instance of a subclass of {@link Location} for which these
+	 *            general tests will be performed.
+	 */
+	public TestLocation(Location testLocation) {
+
+		this.testLocation = testLocation;
+
+	}
+
+	/**
+	 * Tests the {@link Location#enter(RoadUser)} method. This method should
+	 * accept a {@link RoadUser} and if it is;
+	 * <ul>
+	 * <li>Not NULL</li>
+	 * <li>Not already in <code>this</code> {@link Location}</li>
+	 * </ul>
+	 * 
+	 * If the {@link Location#maxQueueSize} is gretater than zero then that will
+	 * be tested also. If there is any additional functionality to this method
+	 * in a subclass of location then this method should be overrided and called
+	 * in the test subclass.
+	 */
+	@Test
+	public void testEnter() {
+
+		testLocation.queue.clear();
+
+		// Create a new road user for the test.
+		RoadUser testSmallCar = new SmallCar_RoadUser();
+
+		// Assert that the test locaton's queue is empty to start with.
+		assertTrue(testLocation.queue.size() == 0);
+
+		// Add a null road user to the location.
+		testLocation.enter(null);
+
+		// Assert that the null road user was not allowed into the location.
+		assertTrue(testLocation.queue.size() == 0);
+
+		// Add the test road user.
+		testLocation.enter(testSmallCar);
+
+		// Assert that the valid road user was added.
+		assertTrue(testLocation.queue.size() == 1);
+
+		// Attempt to add the same road user a second time.
+		testLocation.enter(testSmallCar);
+
+		// Assert that the second instance of the valid road user was not added.
+		assertTrue(testLocation.queue.size() == 1);
+
+		testLocation.queue.clear();
+
+		// If the location has a max queue size.
+		if (testLocation.maxQueueSize > 0) {
+			testMaxQueueSize();
+		}
+
+	}
+
+	/**
+	 * Test the {@link Location#maxQueueSize}. If the
+	 * {@link Location#maxQueueSize} is greater than zero then the
+	 * {@link #testLocation} must be shorter than value.
+	 * 
+	 * @see Location#queue
+	 * @see Location#maxQueueSize
+	 */
+	private void testMaxQueueSize() {
+
+		testLocation.queue.clear();
+
+		// Create a list to hold a collection of road users
+		LinkedList<RoadUser> testQueue = new LinkedList<RoadUser>();
+
+		// Holds the cumulative value of all road users vehicle sizes.
+		double queueSize = 0.0;
+
+		// Iterate until the size of the queue of small cars is longer than the
+		// make queue size.
+		while (testLocation.maxQueueSize > queueSize) {
+
+			// Temporary road user to be added to testQueue
+			RoadUser tempRoadUser = new SmallCar_RoadUser();
+			/*
+			 * Add the size of the temporary road users vehicle to the leng6hof
+			 * the queue then add the road user to the testQueue.
+			 */
+			queueSize += tempRoadUser.getVehicle().size;
+			testQueue.add(tempRoadUser);
+		}
+
+		// Attempt to add each road user in the test queue
+		testQueue.forEach(thisRoadUser -> {
+			testLocation.enter(thisRoadUser);
+		});
+
+		/*
+		 * The loop prior opens adds small cars to the test queue until it
+		 * exceeds the max queue size. This means that if the test location
+		 * functions properly there should be one more road user in the test
+		 * queue than in the test locations queue.
+		 */
+		assertTrue(testLocation.queue.size() == testQueue.size() - 1);
+
+		testLocation.queue.clear();
+	}
+
+	@Test
+	public void testReturnToQueue() {
+
+	}
 
 }
